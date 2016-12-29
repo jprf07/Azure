2b05b1d2a9d8a6b9b9431820465b4a62bcbf51ab
Joao Pedro Feliciano <jprf07@users.noreply.github.com>
2016-12-29 20:16:46 +0000

    Update to te controller version 5

--- JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
+++ JSON/quick-redeploy-unifi-controller/Unifi_Controller.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'deb http://www.ubnt.com/downloads/unifi/debian stable ubiquiti' | sudo tee -a /etc/apt/sources.list.d/100-ubnt.list
+echo "deb http://www.ubnt.com/downloads/unifi/debian unifi5 ubiquiti" | sudo tee -a /etc/apt/sources.list.d/100-ubnt.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv C0A52C50 && sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 sudo apt-get update
 sudo apt-get install unifi -y
